6697a38561e5bac82dcdf657b75cf8339e3a61a4
Tim Malich <tim.malich@gmail.com>
2020-10-10 01:41:15 +0200

    Added my first vue/js unit test

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!**/node_modules/**"
+  ],
+  coverageReporters: [
+    "html",
+    "text-summary"
+  ]
+}
---
+++ src/components/memory/MemoryAlphabet.vue
@@ -0,0 +1,38 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: "MemoryAlphabet",
+  data() {
+    return {
+      cards: {},
+      firstCard: {},
+      secondCard: {},
+      hasFlippedCard: false,
+      lockBoard: false,
+      solvedCards: 0,
+      levels: [
+        {rows: 1, columns: 2},
+        {rows: 2, columns: 2},
+        {rows: 2, columns: 3},
+        {rows: 3, columns: 4},
+        {rows: 4, columns: 4},
+        {rows: 4, columns: 5},
+        {rows: 5, columns: 6},
+        {rows: 6, columns: 6},
+        {rows: 6, columns: 7},
+        {rows: 7, columns: 8},
+        {rows: 8, columns: 8},
+      ]
+    };
+  },
+  created: function () {
+  },
+  methods: {}
+}
+
+</script>
+
+<style scoped>
+</style>
