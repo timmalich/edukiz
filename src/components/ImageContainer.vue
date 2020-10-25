5ffa7d6477248de89f811bdb8e5e8aac923bfaa8
Tim Malich <tim.malich@gmail.com>
2020-10-25 23:52:35 +0100

    Prepared max level UI elements

---
+++ src/components/ImageContainer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="ratio-wrapper-container">
+    <div class="inner">
+      <img :src="src" alt="containerized image">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "ImageContainer",
+  props: ['width', 'height', 'src']
+};
+
+</script>
+
+<style scoped>
+.ratio-wrapper-container {
+  position: relative;
+  width: 100%;
+  min-width: 30pt;
+  height: 100%;
+  min-height: 30pt;
+  border-radius: 15pt;
+  background-color: #a94ed2;
+}
+
+.inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  max-width: 100%;
+  max-height: 100%;
+  overflow: hidden;
+}
+
+.inner > img {
+  margin: auto;
+  width: 100%;
+  height: auto;
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
--- src/components/drag_drop/DDCharacters.vue
+++ src/components/drag_drop/DDCharacters.vue
@@ -1,15 +1,31 @@
 <template>
   <Game nav-back-path="/dragdrop" @previous="previousLevel" @restart="restartGame" @next="nextLevel">
-  <i style="color: greenyellow">IN DEVELOPMENT</i>
+    <div class="drop-section">
+      <ImageContainer class="drop-element" src="img/characters/A.svg"></ImageContainer>
+      <ImageContainer class="drop-element" src="img/characters/B.svg"></ImageContainer>
+      <ImageContainer class="drop-element" src="img/characters/C.svg"></ImageContainer>
+      <ImageContainer class="drop-element" src="img/characters/D.svg"></ImageContainer>
+      <ImageContainer class="drop-element" src="img/characters/E.svg"></ImageContainer>
+    </div>
+    <div class="spacer"></div>
+    <div class="drag-section">
+      <ImageContainer src="img/characters/A.svg"></ImageContainer>
+      <ImageContainer src="img/characters/B.svg"></ImageContainer>
+      <ImageContainer src="img/characters/C.svg"></ImageContainer>
+      <ImageContainer src="img/characters/D.svg"></ImageContainer>
+      <ImageContainer src="img/characters/E.svg"></ImageContainer>
+    </div>
   </Game>
 </template>
 
 <script>
 import Game from "../Game";
+import ImageContainer from "../ImageContainer";
 
 export default {
   name: "BuildWords",
   components: {
+    ImageContainer,
     Game,
   },
   data() {
@@ -36,4 +52,23 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.spacer {
+  height: 20%;
+}
+
+.drop-section, .drag-section {
+  width: 100%;
+  height: 40%;
+  display: grid;
+  grid-gap: 10pt;
+  justify-items: center;
+  align-items: center;
+  position: relative;
+  grid-template-columns: repeat(5, minmax(20pt, 1fr));
+}
+
+.drop-element {
+  filter: grayscale(85%);
+}
+
 </style>
