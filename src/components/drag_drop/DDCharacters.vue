e073ca8706b70a55e27f27f133958f1226bb7849
Tim Malich <tim.malich@gmail.com>
2020-10-26 23:52:15 +0100

    ... dragging and dropping - especially for mobile browser is super horrible...

--- src/components/drag_drop/DDCharacters.vue
+++ src/components/drag_drop/DDCharacters.vue
@@ -9,7 +9,16 @@
     </div>
     <div class="spacer"></div>
     <div class="drag-section">
-      <ImageContainer src="img/characters/A.svg"></ImageContainer>
+      <div v-on:mouseup="end($event)"
+           v-on:touchend="end($event)"
+           v-on:mousemove="move($event)"
+           v-on:touchmove="move($event)"
+           v-on:mousedown="start($event)"
+           v-on:touchstart="start($event)"
+           class="foo"
+      >
+        <ImageContainer src="img/characters/A.svg"></ImageContainer>
+      </div>
       <ImageContainer src="img/characters/B.svg"></ImageContainer>
       <ImageContainer src="img/characters/C.svg"></ImageContainer>
       <ImageContainer src="img/characters/D.svg"></ImageContainer>
@@ -29,12 +38,63 @@ export default {
     Game,
   },
   data() {
-    return {};
+    return {
+      elementSelected: false,
+      elementWidth: undefined,
+      elementHeight: undefined,
+      selectedElement: undefined,
+      backupElement: undefined
+    };
   },
   created: function () {
     this.initGame();
   },
+  /* eslint-disable */
   methods: {
+    start: function (evt) {
+      console.log("start");
+      this.elementSelected = true;
+      this.selectedElement =  evt.currentTarget;
+      this.elementWidth = this.selectedElement.clientWidth;
+      this.elementHeight = this.selectedElement.clientHeight;
+
+      this.backupElement = this.selectedElement.cloneNode(true);
+      // todo
+      this.backupElement.id = this.backupElement.id + "-BackupElementFromDragging"; // todo restore id on the end
+      this.selectedElement.after(this.backupElement);
+      this.backupElement.style.visibility = "hidden";
+
+      this.selectedElement.style.position = "fixed";
+      this.selectedElement.style.width = this.elementWidth + "px";
+      this.selectedElement.style.height = this.elementHeight + "px";
+
+      console.log(this.elementWidth);
+      console.log(this.elementHeight);
+
+      //console.log(evt);
+      //debugger; // eslint-disable-line
+    },
+    move: function (evt) {
+      console.log("move");
+      console.log(evt.targetTouches[0].clientX);
+      console.log(evt.targetTouches[0].clientY);
+      this.selectedElement.style.left = evt.targetTouches[0].clientX + "px";
+      this.selectedElement.style.top = evt.targetTouches[0].clientY + "px";
+      // todo heed mouse / touch position
+      evt.stopPropagation();
+      //debugger; // eslint-disable-line
+    },
+    end: function (evt) {
+      this.elementSelected = false;
+      console.log("end");
+      this.selectedElement.style.width = this.elementWidth;
+      this.selectedElement.style.height = this.elementHeight;
+      //console.log(evt);
+      //debugger; // eslint-disable-line
+      // TODO: create a list of all droppable elements and their coordinates
+      // check with this if they overlap.
+      // if not: restore backup and ID
+    },
     initGame: function () {
       // TODO add something
     },
@@ -54,6 +114,11 @@ export default {
 <style scoped lang="scss">
 .spacer {
   height: 20%;
+}
+
+.foo {
+  width: 100%;
+  height: 100%;
 }
 
 .drop-section, .drag-section {
