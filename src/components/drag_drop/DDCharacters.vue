d1c2c14154cf1d6ba284bf8b6a14e9e7133ddda9
Tim Malich <tim.malich@gmail.com>
2020-10-27 23:35:49 +0100

    ... droppable from draggable - especially for mobile browser is super horrible ...

--- src/components/drag_drop/DDCharacters.vue
+++ src/components/drag_drop/DDCharacters.vue
@@ -1,6 +1,6 @@
 <template>
   <Game nav-back-path="/dragdrop" @previous="previousLevel" @restart="restartGame" @next="nextLevel">
-    <div class="drop-section">
+    <div class="drop-section drag-drop-zone draggable-dropzone--active" ref="dropzone">
       <ImageContainer class="drop-element" src="img/characters/A.svg"></ImageContainer>
       <ImageContainer class="drop-element" src="img/characters/B.svg"></ImageContainer>
       <ImageContainer class="drop-element" src="img/characters/C.svg"></ImageContainer>
@@ -8,17 +8,8 @@
       <ImageContainer class="drop-element" src="img/characters/E.svg"></ImageContainer>
     </div>
     <div class="spacer"></div>
-    <div class="drag-section">
-      <div v-on:mouseup="end($event)"
-           v-on:touchend="end($event)"
-           v-on:mousemove="move($event)"
-           v-on:touchmove="move($event)"
-           v-on:mousedown="start($event)"
-           v-on:touchstart="start($event)"
-           class="foo"
-      >
-        <ImageContainer src="img/characters/A.svg"></ImageContainer>
-      </div>
+    <div class="drag-section drag-drop-zone draggable-dropzone--occupied" ref="dragzone">
+      <ImageContainer class="drag-item" src="img/characters/A.svg"></ImageContainer>
       <ImageContainer src="img/characters/B.svg"></ImageContainer>
       <ImageContainer src="img/characters/C.svg"></ImageContainer>
       <ImageContainer src="img/characters/D.svg"></ImageContainer>
@@ -30,6 +21,7 @@
 <script>
 import Game from "../Game";
 import ImageContainer from "../ImageContainer";
+import { Droppable } from '@shopify/draggable';
 
 export default {
   name: "BuildWords",
@@ -49,12 +41,26 @@ export default {
   created: function () {
     this.initGame();
   },
+  mounted: function() {
+    const droppable = new Droppable([this.$refs.dragzone, this.$refs.dropzone], {
+      draggable: '.drag-item',
+      dropzone: '.drag-drop-zone',
+      mirror: {
+        constrainDimensions: true,
+      },
+    });
+    //debugger; // eslint-disable-line
+    droppable.on('droppable:dropped', () => console.log('droppable:dropped'));
+    droppable.on('droppable:returned', () => console.log('droppable:returned'));
+    console.log("droppable inti");
+    console.log(droppable)
+  },
   /* eslint-disable */
   methods: {
     start: function (evt) {
       console.log("start");
       this.elementSelected = true;
-      this.selectedElement =  evt.currentTarget;
+      this.selectedElement = evt.currentTarget;
       this.elementWidth = this.selectedElement.clientWidth;
       this.elementHeight = this.selectedElement.clientHeight;
 
@@ -94,6 +100,8 @@ export default {
       // TODO: create a list of all droppable elements and their coordinates
       // check with this if they overlap.
       // if not: restore backup and ID
+      // TODO: https://shopify.github.io/draggable/examples/unique-dropzone.html use this or this: https://bevacqua.github.io/dragula/
+      //
     },
     initGame: function () {
       // TODO add something
@@ -112,6 +120,12 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+.drag-item {
+  width: 100%;
+  height: 100%;
+}
+
 .spacer {
   height: 20%;
 }
