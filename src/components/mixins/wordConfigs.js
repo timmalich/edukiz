540460ebb11cb39f92ac29124c69866032b3eb90
Tim Malich <tim.malich@gmail.com>
2020-11-01 01:57:26 +0100

    added more words

--- src/components/mixins/wordConfigs.js
+++ src/components/mixins/wordConfigs.js
@@ -4,17 +4,27 @@ export const wordConfigs = {
       wordConfigs: [
         {
           wordLength: 2,
-          words: [
-            'ja'
-          ]
+          words: [ 'ja', 'au', 'da', 'du', 'ei', 'er', 'es', 'hi', 'Po', 'zu']
         },
         {
           wordLength: 3,
-          words: ['foo']
+          words: ['Opa', 'Oma', 'Zoo', 'Yan', 'Tim']
         },
         {
           wordLength: 4,
-          words: ['Mama', 'Papa']
+          words: ['Mama', 'Papa', 'Huhn', 'Rudi', 'Affe']
+        },
+        {
+          wordLength: 5,
+          words: ['Tiger', 'Pferd', 'Fisch', 'Panda', 'Vogel', 'Fuchs']
+        },
+        {
+          wordLength: 6,
+          words: ['Drache', 'Milina', 'Damian', 'Katrin', 'Gudrun']
+        },
+        {
+          wordLength: 7,
+          words: ['Pinguin', 'Elefant', 'Blauwal', 'Giraffe', 'Gorilla', 'Hamster', 'Nashorn']
         }
       ]
     }
