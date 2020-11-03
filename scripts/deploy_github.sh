485691e542a0994912565eee9f4c00cc668bf334
Tim Malich <tim.malich@gmail.com>
2020-11-03 21:30:23 +0100

    fix success sounds

---
+++ scripts/deploy_github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd "${0%/*}../"
+
+npm run-script build
+cp -r ./dist/* ../edukiz_live/
+COMMIT_ID=$(git rev-parse --short HEAD)
+LATEST_TAG=$(git describe origin --abbrev=0 --tags)
+COMMIT_MSG="Based on edukiz commit: ${COMMIT_ID} and tag: ${LATEST_TAG}"
+
+cd ../edukiz_live/ || exit 1
+git add -A
+git commit -m "${COMMIT_MSG}"
+git tag -a "${LATEST_TAG}" -m "${COMMIT_MSG}"
+git push
+echo -e '\033[0;32m \t Success!!!! Yeah!!! '
